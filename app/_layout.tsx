4bcda866c8f3dac688ef1d16f873d8a30109c2f5
Taewoong Heo <chamjoeun0111@gmail.com>
2025-10-24 15:32:31 +0900

    chore(env): add zod, dotenv

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,31 +1,11 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
+import '@/global.css';
+import { Text, View } from 'react-native';
 import 'react-native-reanimated';
 
-import { useColorScheme } from '@/src/hooks/use-color-scheme';
-import '../global.css';
-
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
-
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
-        <Stack.Screen
-          name='modal'
-          options={{
-            presentation: 'modal',
-            title: 'Modal',
-          }}
-        />
-      </Stack>
-      <StatusBar style='auto' />
-    </ThemeProvider>
+    <View>
+      <Text>Hello World</Text>
+    </View>
   );
 }
