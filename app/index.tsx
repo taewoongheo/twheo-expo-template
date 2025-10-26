f2f757b034c9c018cf934f3618403b85f402e1ca
Taewoong Heo <chamjoeun0111@gmail.com>
2025-10-26 17:50:52 +0900

    feat(ui): integrate design tokens into Tailwind config

--- app/index.tsx
+++ app/index.tsx
@@ -8,7 +8,7 @@ function Index() {
   const { language, setLanguage } = useLanguage();
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', gap: 10 }}>
       <Text style={{ fontSize: 20, fontWeight: 'bold' }}>env: {Env.APP_ENV}</Text>
       <Text style={{ fontSize: 20, fontWeight: 'bold' }}>{Env.NAME}</Text>
       <Text style={{ fontSize: 20, fontWeight: 'bold' }}>{Env.VERSION}</Text>
@@ -17,6 +17,32 @@ function Index() {
         title='Change Language'
         onPress={() => setLanguage(language === 'en' ? 'ko' : 'en')}
       />
+      <View
+        style={{
+          flexDirection: 'row',
+          flexWrap: 'wrap',
+          gap: 10,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        {Array.from({ length: 11 }).map((_, index) => {
+          const colorClass = [
+            'bg-primary-50',
+            'bg-primary-100',
+            'bg-primary-200',
+            'bg-primary-300',
+            'bg-primary-400',
+            'bg-primary-500',
+            'bg-primary-600',
+            'bg-primary-700',
+            'bg-primary-800',
+            'bg-primary-900',
+          ][index];
+
+          return <View className={colorClass} key={index} style={{ width: 50, height: 50 }} />;
+        })}
+      </View>
     </View>
   );
 }
